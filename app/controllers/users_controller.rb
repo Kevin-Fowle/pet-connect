2ca9d153aa2f63bf49ce3eb86877d9c75f6a99b8
Kevin Fowle <email": null>
2016-06-24 16:09:25 -0500

    Add working register user-pet owner form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController <  ApplicationController
+
+def new
+  @user = User.new
+end
+
+def create
+  p user_params
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to 'new_user_pet'
+  else
+    render 'new_user_path'
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :email, :password, :street_address, :city, :zip_code, :state)
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :users, except:[:index] do
+    resources :pets, except:[:index]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
