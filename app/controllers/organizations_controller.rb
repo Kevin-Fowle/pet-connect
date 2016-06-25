34d980b77e3c46a2fa7df3d3e0b255e15bf80f62
Milorad Felbapov <email": null>
2016-06-25 10:30:49 -0500

    search works adding js

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,12 +1,26 @@
 class OrganizationsController < ApplicationController
+  before_action :set_organization, only: [:show]
+
+  def show
+  end
+
   def new
     @organization = Organization.new
   end
 
   def search
-    input = "A"##params[:search]
-    @organizations = Organization.where(["name = (?)", "#{input}%"])
+    input = params[:search]
+    upcase_input = input.upcase
+    if request.xhr?
+      @organizations = Organization.where('name LIKE ?', "#{upcase_input}%").limit(10)
   end
 
+  private
+    def set_organization
+      @organization = Organization.find(params[:id])
+    end
 
+    def organization_params
+      params.require(:oragnization).permit(:name, :street_address, :city, :zip_code, :state, :phone)
+    end
 end
