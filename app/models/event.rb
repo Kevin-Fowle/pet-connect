e9df2c4bcb46f0984b2b288fe389e9d042704d41
SJEble <sjeble@gmail.com>
2016-06-25 16:10:08 -0500

    another schema naming thing

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
   belongs_to :pairing
-  belongs_to :pet_owner_timeslot
+  belongs_to :timeslot
 end
--- db/migrate/20160625191231_create_events.rb
+++ db/migrate/20160625191231_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.references :pairing
-      t.references :pettimeslot
+      t.references :timeslot
 
       t.timestamps null: false
     end
