5dd5f1d02c1a90414561f6f2b57dd08b5e50fb78
Milorad Felbapov <email": null>
2016-06-25 10:51:27 -0500

    keyup works

--- app/assets/javascripts/search_org.js
+++ app/assets/javascripts/search_org.js
@@ -1,5 +1,15 @@
 $(document).ready(function(event){
 
-  $('#new_organization').on
-  event.preventDefault()
+  $('#new_organization').on("submit", function(event){
+    event.preventDefault();
+    var searchForm = $(this);
+
+  })
+
+  $('#organization_name').on("keypress", function(event) {
+    var searchField = $(this)
+    console.log(searchField)
+    var data = searchField.val()
+    console.log(data);
+  })
 })
