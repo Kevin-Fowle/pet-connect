007ca4ba1d7c4d5874aceeb52695d1a59e4789f9
Milorad Felbapov <email": null>
2016-06-26 14:42:38 -0500

    Wrote a method that we're probably not going to use

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -19,4 +19,8 @@ class Organization < ActiveRecord::Base
     end
     conversation_arr
   end
+
+  def approved_users
+    approved_users = self.pairings.map { |pairing| pairing.active }
+  end
 end
